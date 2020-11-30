012873be3130235730809182877926d924c615aa
Alexander Dubrovin <alexanderdubrovin@alexanders-air.home>
2020-11-30 18:42:09 +0000

    Fail CI on linter errors / fixed linting errors

--- src/assets/LeetInput.spec.ts
+++ src/assets/LeetInput.spec.ts
@@ -1,39 +1,39 @@
 import { assert } from "chai";
 import LeetInput from "./LeetInput";
 
-describe('LeetInput', () => {
+describe("LeetInput", () => {
     let leetInput;
 
     beforeEach(() => {
         leetInput = new LeetInput();
     });
 
-    describe('getDescription()', () => {
-        it('should return correct result', () => {
-            assert.strictEqual(leetInput.getDescription(), 'm4k35 y0u2 73x7 100k 11k3 7h15.');
+    describe("getDescription()", () => {
+        it("should return correct result", () => {
+            assert.strictEqual(leetInput.getDescription(), "m4k35 y0u2 73x7 100k 11k3 7h15.");
         });
     });
 
-    describe('getThumbnailUrl()', () => {
-        it('should return correct result', () => {
-            assert.strictEqual(leetInput.getThumbnailUrl(), 'https://lh3.googleusercontent.com/proxy/2zz2eHImhqyePfgOCNVmH9UPqaGktuCKgwzb0L9PcA-jzoB-STitf4nWEOiVvzv7szlN5JLQp6dmSzVAmw3xmGWiKGN0ZKNRzc5xMt6TmmhvsvOI');
+    describe("getThumbnailUrl()", () => {
+        it("should return correct result", () => {
+            assert.strictEqual(leetInput.getThumbnailUrl(), "https://lh3.googleusercontent.com/proxy/2zz2eHImhqyePfgOCNVmH9UPqaGktuCKgwzb0L9PcA-jzoB-STitf4nWEOiVvzv7szlN5JLQp6dmSzVAmw3xmGWiKGN0ZKNRzc5xMt6TmmhvsvOI");
         });
     });
 
-    describe('processMessage()', () => {
-        it('should translate special chars', () => {
-            const message = 'aegiost';
-            const expected = '4361057';
+    describe("processMessage()", () => {
+        it("should translate special chars", () => {
+            const message = "aegiost";
+            const expected = "4361057";
             assert.strictEqual(leetInput.processMessage(message), expected);
         });
-        it('should not translate regular chars', () => {
-            const message = 'bcdfhjklmnpqruvwxyz!(){}';
-            const expected = 'bcdfhjklmnpqruvwxyz!(){}';
+        it("should not translate regular chars", () => {
+            const message = "bcdfhjklmnpqruvwxyz!(){}";
+            const expected = "bcdfhjklmnpqruvwxyz!(){}";
             assert.strictEqual(leetInput.processMessage(message), expected);
         });
-        it('should translate message', () => {
-            const message = 'Makes your text look like this!';
-            const expected = 'm4k35 y0ur 73x7 l00k l1k3 7h15!';
+        it("should translate message", () => {
+            const message = "Makes your text look like this!";
+            const expected = "m4k35 y0ur 73x7 l00k l1k3 7h15!";
             assert.strictEqual(leetInput.processMessage(message), expected);
         });
     });
--- src/assets/LeetInput.ts
+++ src/assets/LeetInput.ts
@@ -1,15 +1,15 @@
-import { IInput } from './IInput';
+import { IInput } from "./IInput";
 
 export default class LeetInput implements IInput {
 
     private static lookup: Record<string, string> = {
-        a: '4',
-        e: '3',
-        g: '6',
-        i: '1',
-        o: '0',
-        s: '5',
-        t: '7'
+        a: "4",
+        e: "3",
+        g: "6",
+        i: "1",
+        o: "0",
+        s: "5",
+        t: "7"
     }
     
     public processMessage(message: string): string {
@@ -22,10 +22,10 @@ export default class LeetInput implements IInput {
     }
     
     public getDescription(): string {
-        return 'm4k35 y0u2 73x7 100k 11k3 7h15.';
+        return "m4k35 y0u2 73x7 100k 11k3 7h15.";
     }
     
     public getThumbnailUrl?(): string {
-        return 'https://lh3.googleusercontent.com/proxy/2zz2eHImhqyePfgOCNVmH9UPqaGktuCKgwzb0L9PcA-jzoB-STitf4nWEOiVvzv7szlN5JLQp6dmSzVAmw3xmGWiKGN0ZKNRzc5xMt6TmmhvsvOI';
+        return "https://lh3.googleusercontent.com/proxy/2zz2eHImhqyePfgOCNVmH9UPqaGktuCKgwzb0L9PcA-jzoB-STitf4nWEOiVvzv7szlN5JLQp6dmSzVAmw3xmGWiKGN0ZKNRzc5xMt6TmmhvsvOI";
     }
 }
